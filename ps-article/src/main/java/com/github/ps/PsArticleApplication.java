93280b6f13654cdd9208a399b03fa9f5d84ad30b
xifeng_alven <xifeng_alven@foxmail.com>
2024-11-12 16:49:11 +0800

    基本的项目结构创建完了，后边就是移动代码了

---
+++ ps-article/src/main/java/com/github/ps/PsArticleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ps;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsArticleApplication.class, args);
+    }
+}
---
+++ ps-user/src/main/java/com/github/ps/user/PsUserApplication.java
@@ -0,0 +1,13 @@
+package com.github.ps.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsUserApplication.class, args);
+    }
+
+}
